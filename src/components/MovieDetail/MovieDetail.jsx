5924e522a21ef81fa9c6d0d84cd4b80adbe635be
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-06-26 15:21:06 -0500

    added material ui button

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -2,6 +2,8 @@ import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import './MovieDetail.css';
+import * as React from 'react';
+import Button from '@mui/material/Button';
 function MovieDetail() {
     const movies = useSelector(store => store.movies);
 
@@ -13,16 +15,19 @@ function MovieDetail() {
         <>
             <div className='container'>
                 <h3>{movies[id].title}</h3>
-                    <img src={movies[id].poster} />
-                
+                <img src={movies[id].poster} />
 
-                <div className='textbox'>
 
-                    <p>{movies[id].description}</p>
-                    <Link to={'/'}>
-                        <button>Go Back</button>
-                    </Link>
-                </div>
+
+
+                <p>{movies[id].description}</p>
+                <Link to={'/'}>
+                    <Button variant="text">
+                        Go Back
+                    </Button>
+                </Link>
+
+
 
 
             </div>
