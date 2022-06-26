14cfcbc11061e02ded9cd3e6a4aff5089234752f
Tarek Azzazi <azzazi.tarek@gmail.com>
2022-06-26 10:34:36 -0500

    detail view working

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -1,14 +1,22 @@
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 function MovieDetail() {
     const movies = useSelector(store => store.movies);
-   
+
     console.log('In movie detail');
-    
     const { id } = useParams();
+
+
     return (
         <>
+            <h3>{movies[id].title}</h3>
+
             <img src={movies[id].poster} />
+            <p>{movies[id].description}</p>
+            <Link to={'/'}>
+                <button>Go Back</button>
+            </Link>
         </>
     )
 
